3fe1ad59e84ffb4228d7b46402cd1067f4b769a4
maicol16 <mchavezhuer@uniminuto.edu.co>
2017-05-07 13:01:38 -0500

    Update Taller2punto1.java

--- src/taller2punto1/Taller2punto1.java
+++ src/taller2punto1/Taller2punto1.java
@@ -1,38 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package taller2punto1;
 
-public class Taller2punto1 {
-
+public class Taller2punto1 
+{
     int minDist(int arr[], int n, int p, int q) 
     {
-        int i, j;
+        int i = 0;
         int min_dist = Integer.MAX_VALUE;
+        int prev=0;
+       
         for (i = 0; i < n; i++) 
-        {
-            for (j = i + 1; j < n; j++) 
+         {
+            if (arr[i] == p || arr[i] == q) 
             {
-                if ((p == arr[i] && q == arr[j]
-                    || q == arr[i] && p == arr[j])
-                    && min_dist > Math.abs(i - j)) 
-                    min_dist = Math.abs(i - j);
+                prev = i;
+                break;
             }
         }
+ 
+        for (; i < n; i++) 
+        {
+            if (arr[i] == p || arr[i] == q) 
+            {
+               
+                if (arr[prev] != arr[i] && (i - prev) < min_dist) 
+                {
+                    min_dist = i - prev;
+                    prev = i;
+                } 
+                else
+                    prev = i;
+            }
+        }
+ 
         return min_dist;
     }
  
-    public static void main(String[] args) 
-    {
-      Taller2punto1 min = new Taller2punto1();
-        int arr[] = {1,2,3,4,5,6};
+    public static void main(String[] args) {
+        Taller2punto1  min = new Taller2punto1 ();
+        int arr[] = {3, 5, 4, 2, 6, 5, 6, 6, 5, 4, 8, 3};
         int n = arr.length;
-        int p = 1;
-        int q = 6;
+        int x = 3;
+        int y = 6;
  
-        System.out.println("Distancia mínima entre" + p + " and " + q 
-                + " is " + min.minDist(arr, n, p, q));
+        System.out.println("la minima distancia es " + x + " and " + y
+                + " is " + min.minDist(arr, n, x, y));
     }
 }
